c480eaee3ed8ce92c36267d1c97a1f6ae19c1379
Bandiu <Bandiubandi@gmail.com>
2023-08-12 16:20:27 +0300

    Create change checking

--- src/Finder.java
+++ src/Finder.java
@@ -10,10 +10,11 @@ public class Finder {
     private String allLines = null;
     public Finder (String link)
     {
-       if (!downloader(link))
+        allLines = downloader(link);
+       if (allLines == null)
            throw new RuntimeException("Cant connect");
     }
-    private boolean downloader(String link)
+    private String downloader(String link)
     {
 //        try {
 //            URL website = new URL(link);
@@ -39,17 +40,19 @@ public class Finder {
         try {
             URL url = new URL(link);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
             int responseCode = httpURLConnection.getResponseCode();
+            String inputLine = null;
             if(responseCode == HttpURLConnection.HTTP_OK)
             {
                 InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8");
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 StringBuilder sb=new StringBuilder();
-                while (bufferedReader.ready()) {
+                while ((inputLine = bufferedReader.readLine()) != null) {
                     sb.append(bufferedReader.readLine()).append("\n");
                 }
                 allLines=sb.toString();
-                return true;
+                return allLines;
             }
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
@@ -58,7 +61,7 @@ public class Finder {
         }
 
 
-        return false;
+        return null;
     }
 
     public String getAllLines() {
