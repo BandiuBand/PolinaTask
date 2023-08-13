312bb833e1017d21ac5001bf80f567e3771f608d
Bandiu <Bandiubandi@gmail.com>
2023-08-13 15:43:27 +0300

    test download first page

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -6,12 +13,29 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+       //Downloader downloader = new Downloader("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Nissan_Tiida_C11_003.JPG/300px-Nissan_Tiida_C11_003.JPG","c:\\","1");
+       //downloader.start();
+       Finder finder = new Finder("https://www.gettyimages.co.uk/photos/travel?assettype=image&license=rf&alloweduse=availableforalluses&embeddable=true&family=creative&phrase=travel&sort=best&page=1");
+//        System.out.println(finder.getAllLines());
+//        try {
+//            FileWriter fileWriter = new FileWriter("C:\\2.txt");
+//            fileWriter.write(finder.getAllLines());
+//            fileWriter.flush();
+//            fileWriter.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        Elements links = finder.getLinks();
+        System.out.println(links.size());
+        String pathDestination = "C:\\pictures\\";
+        int i = 0;
+        for (Element element:links) {
+            String link = element.attr("src");
+            Downloader downloader = new Downloader(link,pathDestination,Integer.toString(i));
+            downloader.start();
+            i++;
+            System.out.println(link);
+            System.out.println();
         }
     }
 }
